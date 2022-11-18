c0555d2c007caa990296638ba34f12d2f9f29411
Joseph Tripodi <au.joseph.tripodi@gmail.com>
2022-11-18 20:26:23 +1030

    Cypress tests for clubFines

---
+++ cypress/e2e/clubFines.cy.js
@@ -0,0 +1,8 @@
+describe("Club fines", () => {
+  it("can navigate to Club Fines", () => {
+    cy.viewport('macbook-13');
+    cy.visit("/");
+    cy.get(".navbar-item").contains("Club Fines").click();
+    cy.get("#search");
+  })
+});
