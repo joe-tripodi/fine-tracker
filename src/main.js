844aa8240e2b258aac9f7246d8d66b6970a20afd
Joseph Tripodi <au.joseph.tripodi@gmail.com>
2022-11-24 07:16:59 +1030

    Adds vuetify to the project

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-/* import specific icons */
-import { fas } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fas);
-
 import database from "./firebase/firebase";
-
 import App from "./App.vue";
 import router from "./router/router";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+import vuetify from './plugins/vuetify'
+
+library.add(fas);
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 app.component('fa', FontAwesomeIcon);
 
 app.provide('database', database);
