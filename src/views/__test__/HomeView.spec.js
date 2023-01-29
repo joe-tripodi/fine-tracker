0597353ea27af12575b13faab515b122b8af34c3
Joseph Tripodi <au.joseph.tripodi@gmail.com>
2023-01-30 08:31:48 +1030

    Fix HomeView test

--- src/views/__test__/HomeView.spec.js
+++ src/views/__test__/HomeView.spec.js
@@ -1,11 +1,18 @@
 import { describe, it, expect } from "vitest";
 
 import { mount } from "@vue/test-utils";
+import { createVuetify } from "vuetify";
 import HomeView from "../HomeView.vue";
+
+const vuetify = createVuetify();
 
 describe("Home View", () => {
   it("exists", () => {
-    const wrapper = mount(HomeView);
+    const wrapper = mount(HomeView, {
+      global: {
+        plugins: [vuetify],
+      }
+    });
     expect(wrapper.exists()).toBe(true);
   });
 });
