4f2760d8b8d8ca93f584c9076410fbdd40afad17
Joseph Tripodi <au.joseph.tripodi@gmail.com>
2023-02-01 07:12:42 +1030

    use material design 3 blueprint

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,11 @@
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
+import { md3 } from 'vuetify/blueprints'
 
 import { createVuetify } from "vuetify";
 
 export default createVuetify({
+  blueprint: md3,
   theme: {
     themes: {
       light: {
