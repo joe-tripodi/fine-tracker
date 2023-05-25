ad26ddec726e86a42627dce9519f8547aee356df
Joseph Tripodi <au.joseph.tripodi@gmail.com>
2023-05-26 08:24:49 +0930

    Add player fine statistics

---
+++ src/components/StatCard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div role="button" class="stat-wrapper">
+    <p class="player-rank">{{ rank }}</p>
+    <div class="player-details">
+      <div class="player-name">{{ player.name }}</div>
+      <div class="number-of-fines">Number of fines: {{ player.numberOfFines }}</div>
+    </div>
+    <p class="total-value">${{ player.totalValue }}</p>
+  </div>
+  
+</template>
+
+<script setup>
+const props = defineProps({
+  player: {},
+  rank: 0,
+});
+
+</script>
+
+<style scoped>
+.stat-wrapper {
+  display: flex;
+  border: 1px solid black;
+  border-radius: 5px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  margin: 10px 30px;
+  box-shadow: 3px 3px 0 black;
+  background-color: #f3d840;
+  font-family: Circular, Roboto, Helvetica, Arial, sans-serif;
+}
+.player-rank {
+  color: white;
+  font-weight: bold;
+  padding: 0px 5px;  
+  border: 1px solid black;
+  border-radius: 25%;
+  background-color: #017d5e;
+  box-shadow: 3px 3px 0 black;
+}
+
+.player-details {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: black;
+}
+
+.player-name {
+  font-weight: bolder;
+  font-size: 20px;
+  padding: 0px;
+  margin: 0px;
+}
+
+.number-of-fines {
+  font-size: 14px;
+  padding: 0px;
+  margin: 0px;
+}
+
+.total-value {
+  font-weight: bolder;
+  text-align: center;
+  color: white;
+  font-size: 18px;
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 25%;
+  background-color: #0d60a8;
+  box-shadow: 3px 3px 0 black;
+}
+</style>
--- src/views/FineStatistics.vue
+++ src/views/FineStatistics.vue
@@ -1,1 +1,51 @@
-<template></template>
+<template>
+  <div>
+    <div>
+      <StatCard
+        v-for="(player, index) in sortedPlayerFineStatistics"
+        :key="index"
+        :rank="index+1"
+        :player="player"
+      />
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import StatCard from "../components/StatCard.vue";
+import { computed, inject, onMounted, ref } from "vue";
+
+const database = inject("database");
+const allPaidFines = ref([])
+const playerFineStatistics = ref({})
+
+onMounted(async () => {
+  await database.getAllPaidFines().then((res) => {
+    allPaidFines.value = res;
+  });
+  allPaidFines.value.forEach(fine => {
+    if(playerFineStatistics.value[fine.playerId]) {
+      playerFineStatistics.value[fine.playerId].numberOfFines += 1
+      playerFineStatistics.value[fine.playerId].totalValue += fine.amount
+    } else {
+      playerFineStatistics.value[fine.playerId] = {
+        numberOfFines: 1,
+        totalValue: fine.amount,
+        name: fine.playerName,
+      }
+    }
+  });
+});
+
+// create a computed function that returns the playerFineStatics as an array that is sorted by totalValue desc
+const sortedPlayerFineStatistics = computed(() => {
+  const sortedPlayerFineStatistics = []
+  for (const playerId in playerFineStatistics.value) {
+    sortedPlayerFineStatistics.push(playerFineStatistics.value[playerId]);
+  }
+  sortedPlayerFineStatistics.sort((playerA, playerB) => (playerA.totalValue < playerB.totalValue ? 1 : -1));
+  return sortedPlayerFineStatistics;
+});
+
+</script>
